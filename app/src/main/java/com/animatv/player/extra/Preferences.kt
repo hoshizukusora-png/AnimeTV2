99361b8e657c34f8fdb8b8200a01878fa60d9232
manakayuuna123-dot <manakayuuna123@gmail.com>
2026-03-22 17:36:57 +0800

    Add files via upload

--- app/src/main/java/com/animatv/player/extra/Preferences.kt
+++ app/src/main/java/com/animatv/player/extra/Preferences.kt
@@ -168,7 +168,7 @@ class Preferences {
         }
 
     var resizeMode: Int
-        get() = preferences.getInt(RESIZE_MODE, 0)
+        get() = preferences.getInt(RESIZE_MODE, 3)
         set(value) {
             editor = preferences.edit()
             editor.putInt(RESIZE_MODE, value)
